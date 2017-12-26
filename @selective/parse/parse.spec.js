c02fa8c52e984693d437cee8bb8a452664879bf6
Christian Murphy <Christian.Murphy.42@gmail.com>
2017-12-26 14:16:41 -0700

    test(parse): add tests for value trimming and for exception handling

--- @selective/parse/parse.spec.js
+++ @selective/parse/parse.spec.js
@@ -1,6 +1,6 @@
 const parse = require("./parse.js");
 
-test("reads a valid rule", () => {
+test("trim single quotes", () => {
   const string =
     ".test {name: 'test-name'; description: 'test description'; recommended: warn}";
   expect(parse(string)).toEqual([
@@ -11,4 +11,27 @@ test("reads a valid rule", () => {
       selector: ".test"
     }
   ]);
+});
+
+test("trim double quotes", () => {
+  const string =
+    '.test {name: "test-name"; description: "test description"; recommended: warn}';
+  expect(parse(string)).toEqual([
+    {
+      name: "test-name",
+      description: "test description",
+      recommended: "warn",
+      selector: ".test"
+    }
+  ]);
+});
+
+test("error on missing curly", () => {
+  const string = ".nope {";
+  expect(() => parse(string)).toThrow();
+});
+
+test("error on unexpected property", () => {
+  const string = ".test { nope: 'should not be here' }";
+  expect(() => parse(string)).toThrow();
 });
