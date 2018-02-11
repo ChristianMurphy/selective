3e6399fd7202ea7ee66b8ca3d68c098683140a6a
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-02-10 20:07:04 -0700

    refactor: use unified lint rule to allow rule configuration

--- @selective/lint/lint.spec.js
+++ @selective/lint/lint.spec.js
@@ -1,5 +1,5 @@
 const lint = require("./lint");
 
-test("should return function", () => {
-  expect(lint([])).toBeInstanceOf(Function);
+test("should return a list of functions", () => {
+  expect(lint([])).toBeInstanceOf(Array);
 });
