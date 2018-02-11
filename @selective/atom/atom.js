fc4ff8ea47644daa1942d08fb2b138c06ef87ffc
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-02-10 20:09:00 -0700

    feat: move atom module back into core

---
+++ @selective/atom/atom.js
@@ -0,0 +1,22 @@
+const engine = require("unified-engine-atom");
+const rehype = require("rehype");
+const selective = require("@selective/rehype");
+
+module.exports.provideLinter = function() {
+  return {
+    grammarScopes: ["text.html", "text.html.basic"],
+    name: "selective",
+    scope: "file",
+    lintsOnChange: true,
+    lint: engine({
+      processor: rehype,
+      plugins: [selective()]
+    })
+  };
+};
+
+module.exports.activate = () => {
+  if (!atom.inSpecMode()) {
+    require("atom-package-deps").install("linter-selective");
+  }
+};
